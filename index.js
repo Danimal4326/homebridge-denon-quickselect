bf1dd0e64d83564cc406c176aaadfe1af07d5410
Danimal4326 <danimal4326@gmail.com>
2021-08-29 16:16:06 -0500

    Modifiy quickselect to toggle between 2 modes.  read current surround mode

--- index.js
+++ index.js
@@ -13,10 +13,12 @@ function DenonQuickselect(log, config) {
     this.log = log
     this.name = config.name
     this.host = config.host
-    this.quickselect = config.quickselect || 1
+    this.quickSelect = config.quickSelect || 1
+    this.defaultQuickSelect = config.defaultQuickSelect
     this.zone = (config.zone || 1) | 0
     this.pingUrl = config.pingUrl
     this.matchSource = config.matchSource
+    this.matchSurroundMode = config.matchSurroundMode
 
     if (this.zone < 1 && this.zone > 2) {
         this.log.warn('Zone number is not recognized (must be 1 or 2) - assuming zone 1')
@@ -51,21 +53,30 @@ DenonQuickselect.prototype.getState = function(callback) {
   const xml = this.buildXml({
     cmd: [
       { attr: { id: '1' }, val: 'GetAllZonePowerStatus' },
-      { attr: { id: '2' }, val: 'GetAllZoneSource' }
+      { attr: { id: '2' }, val: 'GetAllZoneSource' },
+      { attr: { id: '2' }, val: 'GetSurroundModeStatus' }
     ]
   })
   
   this.doRequest(xml).then(resp => {
     const power = resp.rx.cmd[0].zone1[0] === 'ON'
     const source = resp.rx.cmd[1].zone1[0].source[0] === this.matchSource
+    const mode = resp.rx.cmd[2].surround[0].trim() === this.matchSurroundMode
     
-    callback(null, (power && source) ? 1 : 0)
+    if (this.matchSource)
+        powerState = (power && source) ? 1 : 0
+    else if (this.matchSurroundMode)
+        powerState = mode
+    else
+        powerState = power
+ 
+    callback(null, powerState)
   })
   .catch(e => { this.log.warn(e) })
 }
 
 DenonQuickselect.prototype.setQuickSelect = function(value, callback) {
-  if(this.pingUrl) {
+  if(this.pingUrl && value) {
     request.get(this.pingUrl, (error, response, body) => {
       this.log(`pinged ${this.pingUrl} - got: ${error} and ${response.statusCode}}`)
     })
@@ -74,11 +85,11 @@ DenonQuickselect.prototype.setQuickSelect = function(value, callback) {
   const xml = this.buildXml({
     cmd: { attr: { id: '1' }, val: 'SetQuickSelect' },
     zone: this.zoneName,
-    value: this.quickselect
+    value: (value ? this.quickSelect : this.defaultQuickSelect)
   })
 
   this.doRequest(xml).then(resp => {
-    this.log('switch to ', this.quickselect, this.zoneName, JSON.stringify(resp.rx))
+    this.log('switch to quickSelect:', (value ? this.quickSelect : this.defaultQuickSelect), " zone: ", this.zoneName, " resp: ", JSON.stringify(resp.rx))
     callback()
   })
   .catch(e => { this.log.warn(e) })
@@ -89,7 +100,7 @@ DenonQuickselect.prototype.getServices = function() {
   informationService
     .setCharacteristic(Characteristic.Manufacturer, 'Denon')
     .setCharacteristic(Characteristic.Model, 'Quickselect')
-    .setCharacteristic(Characteristic.SerialNumber, 'QS ' + this.quickselect)
+    .setCharacteristic(Characteristic.SerialNumber, 'QS ' + this.quickSelect)
 
   var switchService = new Service.Switch(this.name)
   switchService
